34d8f9dd5c76db7c885e5424b1bd0c0742c045bd
Lim Jeong Muk <hackerljm@gmail.com>
2018-06-16 00:41:18 +0900

    first commit

---
+++ jmlim-signup/src/main/java/com/jmlim/signup/JmlimSignupApplication.java
@@ -0,0 +1,12 @@
+package com.jmlim.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmlimSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmlimSignupApplication.class, args);
+	}
+}
